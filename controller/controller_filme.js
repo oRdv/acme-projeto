0a6b747a4485aa563b894688c5d15ef1a3fc3da1
Tamires <tamiressenaisp@gmail.com>
2024-02-03 12:12:59 -0300

    finalizado?

---
+++ controller/controller_filme.js
@@ -0,0 +1,34 @@
+// Arquivo responsavel por realizar validacoes, consistencias e regra de negocio para os filmes
+
+//Função para inserir um novo filme
+const setInserirNovoFilme = async function(){
+
+}
+
+//Função para atualizar um filme existente
+const setAtualizarFilme = async function(){
+
+}
+
+//Função para deletar um filme existente
+const setExcluirFilme = async function(){
+
+}
+
+//Função para listar todos os filmes existentes
+const getListarFilmes = async function(){
+
+}
+
+//Função para retornar um filme a partir de um critério (ID) 
+const getBuscarFilme = async function(){
+
+}
+
+module.exports = {
+    setInserirNovoFilme,
+    setAtualizarFilme,
+    setExcluirFilme,
+    getListarFilmes,
+    getBuscarFilme
+}
---
+++ module/DAO/filme.js
@@ -0,0 +1,34 @@
+// arquivo que realiza o CRUD no banco de dados MYSQL para os filmes
+
+//Função para inserir um novo filme no banco de dados
+const insertNovoFilme = async function(){
+
+}
+
+//Função para atualizar um filme existente no banco de dados
+const updateFilme = async function(){
+
+}
+
+//Função para deletar um filme existente no banco de dados
+const deleteFilme = async function(){
+
+}
+
+//Função para listar todos os filmes existentes no banco de dados
+const selectAllFilmes = async function(){
+
+}
+
+//Função para retornar um filme a partir de um critério (ID) no banco de dados
+const selectByIdFilme = async function(){
+
+}
+
+module.exports = {
+    insertNovoFilme,
+    updateFilme,
+    deleteFilme,
+    selectAllFilmes,
+    selectByIdFilme
+}
